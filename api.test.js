2b399b172777a38993dbce04344185e2d6dce2ee
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-02 15:45:15 -0400

    test: remove comments from api test

--- api.test.js
+++ api.test.js
@@ -1,24 +1,18 @@
-import { Server } from "node:http";
 import { describe, before, after, it } from "node:test";
 import assert from "node:assert";
 
 const BASE_URL = "http://localhost:3000";
 
-describe("Login API", () => {
-  /** @type {Server} */
+describe("/login", () => {
+  /** @type {import("node:http").Server} */
   let _server = {};
   let _globalToken = "";
 
-  // Antes de rodar os testes, inicializar o servidor.
-  // Neste caso está sendo utilizado dynamic import.
-  // Os testes só conseguirão rodar depois da API estar rodando (listening).
   before(async () => {
     _server = (await import("./api.js")).app;
     await new Promise((resolve) => _server.once("listening", resolve));
   });
 
-  // Por causa do live reloading, temos que derrubar o servidor para subí-lo
-  // novamente com as novas alterações.
   after((done) => _server.close(done));
 
   it("should receive not authorized given wrong user and password", async () => {
